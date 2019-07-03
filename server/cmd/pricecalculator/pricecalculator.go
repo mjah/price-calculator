06bb7501ed94472a3fa868286bc80fcf1eb120b7
Moe <muhammad.2712@outlook.com>
2019-07-04 00:51:51 +0100

    Added pricecalculator package

---
+++ server/cmd/pricecalculator/pricecalculator.go
@@ -0,0 +1,130 @@
+package pricecalculator
+
+import (
+	ec "../errors"
+)
+
+// PriceCalculator stores the price, costs, and fees
+type PriceCalculator struct {
+	SellPrice             float64 `json:"sell_price"`
+	FreeDeliveryPrice     float64 `json:"free_delivery_price"`
+	Cost                  float64 `json:"cost"`
+	SalesTaxFeeRate       float64 `json:"fees.sales_tax.rate"`
+	PaymentFeeRate        float64 `json:"fees.payment.rate"`
+	PaymentFixedFee       float64 `json:"fees.payment.fixed"`
+	ChannelFeeRate        float64 `json:"fees.channel.rate"`
+	ChannelFixedFee       float64 `json:"fees.channel.fixed"`
+	ChannelFeeIsCapped    bool    `json:"fees.channel.is_capped"`
+	ChannelFeeCappedValue float64 `json:"fees.channel.capped_value"`
+	OtherFeeRate          float64 `json:"fees.other.rate"`
+	OtherFixedFee         float64 `json:"fees.other.fixed"`
+}
+
+// New creates and returns a new instance of PriceCalculator
+func New() *PriceCalculator {
+	return &PriceCalculator{}
+}
+
+// GetSellPriceByProfitRate calculates and returns the sell price given the profit rate
+func (pc *PriceCalculator) GetSellPriceByProfitRate(profitRate float64) (float64, error) {
+	numerator := pc.Cost
+	numerator += pc.FreeDeliveryPrice
+	numerator += pc.PaymentFixedFee
+	numerator += pc.ChannelFixedFee
+	numerator += pc.OtherFixedFee
+
+	denominator := (1 / (1 + pc.SalesTaxFeeRate))
+	denominator -= pc.PaymentFeeRate
+	denominator -= pc.ChannelFeeRate
+	denominator -= pc.OtherFeeRate
+	denominator -= profitRate
+
+	sellPriceUncappedFees := numerator / denominator
+
+	if !pc.ChannelFeeIsCapped || pc.ChannelFeeCappedValue == 0 {
+		if denominator <= 0 {
+			err := ec.New(ec.NegDenUncappedFees, "Negative denomiator with uncapped fees.")
+			return 0, err
+		}
+		return sellPriceUncappedFees, nil
+	} else if denominator+pc.ChannelFeeRate <= 0 {
+		err := ec.New(ec.NegDenCappedFees, "Negative denominator with capped fees.")
+		return 0, err
+	}
+
+	channelFees := sellPriceUncappedFees*pc.ChannelFeeRate + pc.ChannelFixedFee
+
+	if channelFees < pc.ChannelFixedFee || channelFees > pc.ChannelFeeCappedValue {
+		channelFees = pc.ChannelFeeCappedValue
+	}
+
+	sellPriceCappedFees := (numerator + channelFees) / (denominator + pc.ChannelFeeRate)
+
+	return sellPriceCappedFees, nil
+}
+
+// GetFeesTotal calculates and returns the total fees
+func (pc *PriceCalculator) GetFeesTotal() float64 {
+	feesTotal := pc.GetSalesTaxFeesTotal()
+	feesTotal += pc.GetChannelFeesTotal()
+	feesTotal += pc.GetPaymentFeesTotal()
+	feesTotal += pc.GetOtherFeesTotal()
+	return feesTotal
+}
+
+// GetSalesTaxFeesTotal calculates and returns the sales tax
+func (pc *PriceCalculator) GetSalesTaxFeesTotal() float64 {
+	return pc.SellPrice * (1 - (1 / (1 + pc.SalesTaxFeeRate)))
+}
+
+// GetPaymentFeesTotal calculates and returns the payments fees
+func (pc *PriceCalculator) GetPaymentFeesTotal() float64 {
+	return pc.SellPrice*pc.PaymentFeeRate + pc.PaymentFixedFee
+}
+
+// GetChannelFeesTotal calculates and returns the channel fees
+func (pc *PriceCalculator) GetChannelFeesTotal() float64 {
+	channelFeesUncapped := pc.SellPrice*pc.ChannelFeeRate + pc.ChannelFixedFee
+	channelFees := channelFeesUncapped
+
+	if pc.ChannelFeeIsCapped &&
+		pc.ChannelFeeCappedValue != 0 &&
+		channelFeesUncapped > pc.ChannelFeeCappedValue {
+		channelFees = pc.ChannelFeeCappedValue
+	}
+
+	return channelFees
+}
+
+// GetOtherFeesTotal calculates and returns the other fees
+func (pc *PriceCalculator) GetOtherFeesTotal() float64 {
+	return pc.SellPrice*pc.OtherFeeRate + pc.OtherFixedFee
+}
+
+// GetProfitTotal calculates and returns the profit total
+func (pc *PriceCalculator) GetProfitTotal() float64 {
+	profitTotal := pc.SellPrice
+	profitTotal -= pc.GetFeesTotal()
+	profitTotal -= pc.Cost
+	profitTotal -= pc.FreeDeliveryPrice
+	return profitTotal
+}
+
+// IsValidProfitRate checks if the profit rate is valid
+func (pc *PriceCalculator) IsValidProfitRate(profitRate float64) bool {
+	denominator := (1 / (1 + pc.SalesTaxFeeRate))
+	denominator -= pc.PaymentFeeRate
+	denominator -= pc.ChannelFeeRate
+	denominator -= pc.OtherFeeRate
+	denominator -= profitRate
+
+	if !pc.ChannelFeeIsCapped || pc.ChannelFeeCappedValue == 0 {
+		if denominator <= 0 {
+			return false
+		}
+	} else if denominator+pc.ChannelFeeRate <= 0 {
+		return false
+	}
+
+	return true
+}
---
+++ server/cmd/pricecalculator/pricecalculator_test.go
@@ -0,0 +1,161 @@
+package pricecalculator
+
+import (
+	"testing"
+)
+
+type SetExpected struct {
+	set      PriceCalculator
+	expected float64
+}
+
+type SetProfitExpected struct {
+	set        PriceCalculator
+	profitRate float64
+	expected   float64
+}
+
+type SetProfitExpectedBool struct {
+	set        PriceCalculator
+	profitRate float64
+	expected   bool
+}
+
+func Set1() *PriceCalculator {
+	pc := New()
+	pc.SellPrice = 2500
+	pc.SalesTaxFeeRate = 0.20
+	pc.FreeDeliveryPrice = 0
+	pc.Cost = 100
+	pc.PaymentFeeRate = 0.034
+	pc.PaymentFixedFee = 0.20
+	pc.ChannelFeeRate = 0.09
+	pc.ChannelFeeIsCapped = true
+	pc.ChannelFeeCappedValue = 20
+	return pc
+}
+
+func TestGetSellPriceByProfitRate(t *testing.T) {
+	pc := Set1()
+
+	tcs := []SetProfitExpected{
+		{*pc, 0.79933, 36059999.999463506},
+	}
+
+	for _, tc := range tcs {
+		result, err := pc.GetSellPriceByProfitRate(tc.profitRate)
+
+		if err != nil {
+			t.Errorf(err.Error())
+		}
+
+		if result != tc.expected {
+			t.Errorf("Expected sell price of %f got %f.", tc.expected, result)
+		}
+	}
+}
+
+func TestGetFeesTotal(t *testing.T) {
+	pc := Set1()
+
+	tcs := []SetExpected{
+		{*pc, 521.8666666666666},
+	}
+
+	for _, tc := range tcs {
+		result := pc.GetFeesTotal()
+		if result != tc.expected {
+			t.Errorf("Expected total fees of %f got %f.", tc.expected, result)
+		}
+	}
+}
+
+func TestGetSalesTaxFeesTotal(t *testing.T) {
+	pc := Set1()
+
+	tcs := []SetExpected{
+		{*pc, 416.6666666666666},
+	}
+
+	for _, tc := range tcs {
+		result := pc.GetSalesTaxFeesTotal()
+		if result != tc.expected {
+			t.Errorf("Expected sales tax fees of %f got %f.", tc.expected, result)
+		}
+	}
+}
+
+func TestGetPaymentFeesTotal(t *testing.T) {
+	pc := Set1()
+
+	tcs := []SetExpected{
+		{*pc, 85.2},
+	}
+
+	for _, tc := range tcs {
+		result := pc.GetPaymentFeesTotal()
+		if result != tc.expected {
+			t.Errorf("Expected payment fees of %f got %f.", tc.expected, result)
+		}
+	}
+}
+
+func TestGetChannelFeesTotal(t *testing.T) {
+	pc := Set1()
+
+	tcs := []SetExpected{
+		{*pc, 20},
+	}
+
+	for _, tc := range tcs {
+		result := pc.GetChannelFeesTotal()
+		if result != tc.expected {
+			t.Errorf("Expected channel fees of %f got %f.", tc.expected, result)
+		}
+	}
+}
+
+func TestGetOtherFeesTotal(t *testing.T) {
+	pc := Set1()
+
+	tcs := []SetExpected{
+		{*pc, 0},
+	}
+
+	for _, tc := range tcs {
+		result := pc.GetOtherFeesTotal()
+		if result != tc.expected {
+			t.Errorf("Expected other fees of %f got %f.", tc.expected, result)
+		}
+	}
+}
+
+func TestGetProfitTotal(t *testing.T) {
+	pc := Set1()
+
+	tcs := []SetExpected{
+		{*pc, 1878.1333333333334},
+	}
+
+	for _, tc := range tcs {
+		result := pc.GetProfitTotal()
+		if result != tc.expected {
+			t.Errorf("Expected total profit of %f got %f.", tc.expected, result)
+		}
+	}
+}
+
+func TestIsValidProfitRate(t *testing.T) {
+	pc := Set1()
+
+	tcs := []SetProfitExpectedBool{
+		{*pc, 0.79934, false},
+	}
+
+	for _, tc := range tcs {
+		result := pc.IsValidProfitRate(tc.profitRate)
+		if result != tc.expected {
+			t.Errorf("Expected profit rate validity of %t got %t.", tc.expected, result)
+		}
+	}
+}
